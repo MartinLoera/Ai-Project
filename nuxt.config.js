1be8fc3b8649b64ece8652fbddb139359202b142
Martin Loera <jesusmartinloera@gmail.com>
2022-11-28 10:16:16 -0700

    Server config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  server: {
+    host: '0.0.0.0', // default: localhost
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - ai-front',
